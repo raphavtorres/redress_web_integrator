e9b8b8dcde04572967ae6ee68b7e2366d2769fc5
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-22 19:53:14 +0000

    testing

--- web/main.js
+++ web/main.js
@@ -1,47 +1,13 @@
-l_desc = ['fsdfsd']
-l_preco = []
-l_imagens = []
-l_names_prod []
+var name_products = ["Calça", "Blusa"]
+var price_products = [200.00, 90.00]
+var images_products = ["images/card-img-1.webp", "images/card-img-1.webp"]
+var desc_products = ["Description1", "Description2"]
 
+function createModal(item) {
+    var name_elem = document.querySelector("#name_element");
+    var price_elem = document.querySelector("#price_elem");
+    var image_elem = document.querySelector("#image_elem");
+    var desc_elem = document.querySelector("#desc_elem");
 
-// <div class="div-img-details">
-//     <div class="prod-and-price-modal">
-//         <h1 class="title-modal">Product Name</h1>
-//         <h2 class="price-modal">R$ 200,00</h2>
-//     </div>
-//     <div class="div-detail-modal">
-//         <h3 class="detail-modal">Details</h3>
-//         <p>non dignissimos sequi nemo. Animi modi provident laudantium, omnis quo amet nihil
-//             fugiat
-//             autem dolor molestias a tempore possimus ratione id cumque doloremque suscipit rem.
-//         </p>
-//     </div>
-//     <div class="div-sizes-modal">
-//         <h3>Size</h3>
-//         <div class="form-check form-check-inline">
-//             <input class="form-check-input" type="radio" id="p" name="radio" value="p" checked>
-//             <label class="form-check-label" for="p">P</label>
-//         </div>
-
-//         <div class="form-check form-check-inline">
-//             <input class="form-check-input" type="radio" id="m" name="radio" value="m">
-//             <label class="form-check-label" for="m">M</label>
-//         </div>
-
-//         <div class="form-check form-check-inline">
-//             <input class="form-check-input" type="radio" id="g" name="radio" value="g">
-//             <label class="form-check-label" for="g">G</label>
-//         </div>
-//         <div class="form-check form-check-inline">
-//             <input class="form-check-input" type="radio" id="gg" name="radio" value="gg">
-//             <label class="form-check-label" for="gg">GG</label>
-//         </div>
-//     </div>
-//     <div class="div-btn-model">
-//         <button class="btn btn-add-cart-modal">ADD TO CART</button>
-//     </div>
-//     <div class="div-img-icon-model">
-//         <img src="images/icon-delivery truck.svg" alt="">
-//         <p>Free shipping</p>
-//     </div>
-// </div>
+    name_elem.innerHTML = name_products[item];
+}

58c217a5b4761931854dcb346d1f8e7311b39e9d
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-23 10:41:25 -0300

    modal detail creation is now dynamic

--- web/main.js
+++ web/main.js
@@ -1,7 +1,19 @@
-var name_products = ["Calça", "Blusa"]
-var price_products = [200.00, 90.00]
-var images_products = ["images/card-img-1.webp", "images/card-img-1.webp"]
-var desc_products = ["Description1", "Description2"]
+var name_products = ["Jaqueta", "Camiseta Dry Fit", "2", "3", "4", "5", "6"]
+var price_products = [200.00, 90.00, 2, 3, 4, 5, 6]
+var images_products = [
+    "images/card-img-1.webp", 
+    "images/card-img-2.webp", 
+    "images/card-img-2.webp", 
+    "images/card-img-2.webp",
+    "images/card-img-2.webp",
+    "images/card-img-2.webp",
+    "images/card-img-2.webp",
+]
+var desc_products = [
+    "Essa é uma jaqueta",
+    "Essa é uma camiseta para treino",
+    "2", "3", "4", "5", "6"
+]
 
 function createModal(item) {
     var name_elem = document.querySelector("#name_element");
@@ -10,4 +22,7 @@ function createModal(item) {
     var desc_elem = document.querySelector("#desc_elem");
 
     name_elem.innerHTML = name_products[item];
+    price_elem.innerHTML = "R$ " + price_products[item];
+    image_elem.src = images_products[item];
+    desc_elem.innerHTML = desc_products[item];
 }

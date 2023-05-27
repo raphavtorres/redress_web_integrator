0eecf2ee8068b6f77daea8afd707dfe579e92dc9
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-27 14:09:52 -0300

    java reading csv file, creating window with table

--- java/CSVReader/src/App.java
+++ java/CSVReader/src/App.java
@@ -1,23 +1,50 @@
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String file = "C:\\Users\\rapha\\Desktop\\redress_web_integrator1\\RedressData.csv";
+        String path = "C:\\Users\\rapha\\Desktop\\redress_web_integrator1\\RedressData.csv";
         BufferedReader reader = null;
         String line = "";
 
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            // coutinues to read next line
-            while ((line = reader.readLine()) != null) {
-                String[] row = line.split(",");
+        ArrayList<String[]> elementsList = new ArrayList<String[]>();
 
-                for (String index : row) {
-                    System.out.printf("%10s", index);
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+            int i = 0;
+            String[] column = {};
+            while ((line = reader.readLine()) != null) {
+                if (i > 0) {
+                    String[] row = line.split(",");
+                    elementsList.add(row);
                 }
-                System.out.println();
+                else {
+                    column = line.split(",");
+                }    
+                i++;
+                
             }
+            String[][] elementsArray = elementsList.toArray(new String[0][]);
+
+            System.out.println(Arrays.toString(elementsArray));
+
+            JTable table = new JTable(elementsArray, column);
+            JScrollPane scrollPane = new JScrollPane(table);
+    
+            JFrame frame = new JFrame();
+            frame.add(scrollPane);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -27,3 +54,4 @@ public class App {
         }
     }
 }
+

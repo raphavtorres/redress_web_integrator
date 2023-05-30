99dabe34ec2fb2fd2cf98cc3f188d4c26cdeae22
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-30 09:41:35 -0300

    java sending right cpf

--- java/CSVExcel/src/main/java/org/example/WriteExcel.java
+++ java/CSVExcel/src/main/java/org/example/WriteExcel.java
@@ -8,8 +8,19 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 public class WriteExcel {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String cep = "";
+        String pathJson = new File("src\\main\\java\\org\\example\\info.json").getAbsolutePath();
+        String jsonString = String.join(" ", Files.readAllLines(Paths.get(pathJson), StandardCharsets.UTF_8));
+        cep = jsonString.replaceAll("\\D", "");
+
         try {
             // READING EXCEL FILE
             String path = new File("src\\main\\java\\org\\example\\RedressRoutes.xlsx").getAbsolutePath();
@@ -17,15 +28,10 @@ public class WriteExcel {
             FileInputStream arquivo = new FileInputStream(path);
             XSSFWorkbook workbook = new XSSFWorkbook(arquivo);
 
-            // GETTING SHEET
             XSSFSheet sheet = workbook.getSheetAt(0);
-
-            // GETTING CELL
             Row row = sheet.getRow(4);
             Cell cell = row.getCell(2);
-
-            // CHANGING CELL VALUE
-            cell.setCellValue("SALVE");
+            cell.setCellValue(cep);
 
             // SAVING A NEW FILE WITH MODIFICATIONS
             String fileExcel = new File("src\\main\\java\\RedressRoutesResult.xlsx").getAbsolutePath();
